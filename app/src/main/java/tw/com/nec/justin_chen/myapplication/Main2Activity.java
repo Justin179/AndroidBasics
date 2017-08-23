808fbe66ac7d92b5c06771349c3eeafb5258f299
justin-chen <justin.uob@gmail.com>
2017-08-23 13:36:20 +0800

    use Intent to call another Activity

---
+++ app/src/main/java/tw/com/nec/justin_chen/myapplication/Main2Activity.java
@@ -0,0 +1,30 @@
+package tw.com.nec.justin_chen.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        button = (Button)findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(Main2Activity.this,MainActivity.class);
+                startActivity(intent);
+                Main2Activity.this.finish();
+            }
+        });
+
+    }
+}
